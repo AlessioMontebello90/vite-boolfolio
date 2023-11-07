c9f0df72d159aafc25d2effff9781cb7666235ac
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-11-07 18:40:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import ProjectList from "./components/projects/ProjectList.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: { ProjectList },
+};
+</script>
+
+<template>
+  <h1 class="container my-5">hello world</h1>
+  <ProjectList />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h1 class="container my-5 debug">Our projects in Boolean</h1>
+</template>
+
+<style lang="scss" scoped></style>
