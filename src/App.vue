aeb562af9c78469c5725906c6a2c581e2f1046ef
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-11-07 18:45:30 +0100

    added axion and store.js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <script>
 import ProjectList from "./components/projects/ProjectList.vue";
+import axios from "axios";
+import { store } from "./data/store";
 export default {
   data() {
-    return {};
+    return {
+      store,
+      projects: [],
+    };
   },
   components: { ProjectList },
+  methods: {
+    fetchProjects() {
+      axios.get("http://localhost:8000/api/projects").then((response) => {
+        this.projects = response;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
 };
 </script>
 
 <template>
-  <h1 class="container my-5">hello world</h1>
+  <div class="container">
+    <h1 class="my-5">My Boolfolio</h1>
+    {{ projects }}
+    <ProjectList />
+  </div>
   <ProjectList />
 </template>
 
